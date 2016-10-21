b7d3ada5d1cb03de86bf7e0c2545b49f5c40d04d
Lilia <geertszelia@gmail.com>
2016-10-21 16:34:59 +0200

    Affichage des erreurs et page valide fini, correction des bug à finir

--- app.js
+++ app.js
@@ -4,20 +4,21 @@
 		month: null,
 		year: null,
 		date: null,
-		error:null,
+		error: false,
 		
 		init: function(){
 			this.listeners();
 		},
 
 		listeners: function(){
-			$('button').on('click', this.start.bind(this));
+			$('#bouton').on('click', this.start.bind(this));
+			$('#restart').on('click', this.reset.bind(this));
 		},
 
 		start: function(){
 			moment.locale('fr');
 			this.userDate();
-			$('body').html(moment(this.date).format('dddd'));
+			this.message();
 		},
 
 		userDate: function(){
@@ -25,8 +26,42 @@
 			this.month = $('select').val();
 			this.year = parseInt($('#annee').val(), 10);
 			this.date = moment({year: this.year, month: this.month, day: this.day});
+		},
+
+		errorDay: function(){
+			error = true;
+
+			if(this.day < 1 || this.day > 31){
+				$('.error').show().append('Le jour doit être compris entre 1 et 31 !');
+				$('#jour').css('border',"2px solid #ff7473");
+			}
+		},
+
+		errorYear: function(){
+			error = true;
+
+			if(this.year < 0){
+				$('.error').show().append("L'année ne doit pas être inférieur à 0 !");
+				$('#annee').css('border', "2px solid #ff7473");
+			} 
+		},
+
+		message: function(){
+			if(error === true){
+				errorDay();
+				errorYear();
+			}
+			else if(error === false){
+				$('.container').hide();
+				$('.overlay').show();
+				$('#date').text(moment(this.date).format('dddd'));
+			}
 		}
 
+		reset: function(){
+			$('.overlay').hide();
+			$('.container').show();
+		}
 	}
 	app.init();
 
